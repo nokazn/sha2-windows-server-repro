140817ecab5b1c54db227901d2a5ae3b81312cf6
nokazn <41154684+nokazn@users.noreply.github.com>
2024-03-22 01:20:22 +0900

    feat: add other encoding formats tests

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ type Result<T> = result::Result<T, Error>;
 
 fn generate_hash(path: impl AsRef<Path>) -> Result<String> {
   let mut file = fs::File::open(path).map_err(to_error)?;
+  dbg!(file.metadata().unwrap());
   let mut hasher = Sha256::new();
   io::copy(&mut file, &mut hasher).map_err(to_error)?;
   let raw_hash = hasher.finalize();
@@ -46,9 +47,17 @@ mod tests {
   test_each!(
     test_generate_hash,
     test_generate_hash_each,
-    "utf-8" => TestCase{
-      input: "./tests/fixtures/empty.txt",
+    "utf8" => TestCase{
+      input: "./tests/fixtures/empty-utf8.txt",
       expected: "seoc8gkovge196nruj49irtp4gjqsgf4cidp6j54imchmu2in1ag"
+    },
+    "utf16be" => TestCase{
+      input: "./tests/fixtures/empty-utf16be.txt",
+      expected: "u6bmia0gqhbu55vst72maeo2b0fvj6iggkhn1sktfpfu8vct6vj0"
+    },
+    "utf16le" => TestCase{
+      input: "./tests/fixtures/empty-utf16le.txt",
+      expected: "mfah1ro44teahpkosmpsnc7csea9tu955s6dp0sujrhkeg4q484g"
     },
   );
 }
